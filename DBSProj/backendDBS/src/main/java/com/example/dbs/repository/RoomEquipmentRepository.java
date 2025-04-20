1a9af0ed3bc7dff0d075eefdebdca3774b1ac0b3
ParallelParking <avinashbinand@gmail.com>
2025-04-20 12:31:31 +0530

    added room equipment service, controller

--- DBSProj/backendDBS/src/main/java/com/example/dbs/repository/RoomEquipmentRepository.java
+++ DBSProj/backendDBS/src/main/java/com/example/dbs/repository/RoomEquipmentRepository.java
@@ -8,5 +8,8 @@ import com.example.dbs.model.RoomEquipment;
 import com.example.dbs.model.RoomEquipmentId;
 
 public interface RoomEquipmentRepository extends JpaRepository<RoomEquipment, RoomEquipmentId> {
+    
     List<RoomEquipment> findByType(String type);
+
+    List<RoomEquipment> findByBlockAndRoom(String block, String room);
 }
