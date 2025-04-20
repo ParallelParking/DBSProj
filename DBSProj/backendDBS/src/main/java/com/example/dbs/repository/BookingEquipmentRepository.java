aef91494a9be96e1bef3a70aef6bd8c33a220cab
ParallelParking <avinashbinand@gmail.com>
2025-04-20 12:58:01 +0530

    added booking equipment functionality to booking controller, service

--- DBSProj/backendDBS/src/main/java/com/example/dbs/repository/BookingEquipmentRepository.java
+++ DBSProj/backendDBS/src/main/java/com/example/dbs/repository/BookingEquipmentRepository.java
@@ -1,5 +1,6 @@
 package com.example.dbs.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,8 @@ import com.example.dbs.model.BookingEquipment;
 import com.example.dbs.model.BookingEquipmentId;
 
 public interface BookingEquipmentRepository extends JpaRepository<BookingEquipment, BookingEquipmentId> {
+    
     List<BookingEquipment> findByEquipmentType(String equipmentType);
+
+    List<BookingEquipment> findByBlockAndRoomAndDateTime(String block, String room, LocalDateTime dateTime);
 }
