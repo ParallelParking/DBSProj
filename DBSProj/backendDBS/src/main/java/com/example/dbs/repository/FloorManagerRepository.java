4abfb2e068ae919e4eede6b6507705b4f499d03b
ParallelParking <avinashbinand@gmail.com>
2025-04-23 19:27:42 +0530

    added all repo queries

--- DBSProj/backendDBS/src/main/java/com/example/dbs/repository/FloorManagerRepository.java
+++ DBSProj/backendDBS/src/main/java/com/example/dbs/repository/FloorManagerRepository.java
@@ -1,11 +1,8 @@
 package com.example.dbs.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.dbs.model.FloorManager;
 
 public interface FloorManagerRepository extends JpaRepository<FloorManager, String> {
-    Optional<FloorManager> findByEmail(String email);
 }
--- DBSProj/backendDBS/src/main/java/com/example/dbs/repository/UserRepository.java
+++ DBSProj/backendDBS/src/main/java/com/example/dbs/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.example.dbs.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.dbs.model.Users;
 
 public interface UserRepository extends JpaRepository<Users, String> {
 
+    @Query("SELECT u FROM Users u WHERE u.email = :email")
     Optional<Users> findByEmail(String email);
 
 }
